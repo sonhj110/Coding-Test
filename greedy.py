0b3bf63b3380aefad26a6c88182da631e4e4b913
hyojung <sonhj10318@gmail.com>
2023-10-04 20:17:21 +0900

    주석 수정

--- greedy.py
+++ greedy.py
@@ -15,10 +15,9 @@ for coin in coin_types :   # 시간 복잡도는 coin_types의 개수에 달림
 print(count)
 
 
+
+
 # 문제 1 큰 수의 법칙
-## n개의 자연수로 구성된 배열이 있을때 총 m번 더하고 가장 큰 수를 만들어라
-## 이때 같은 수를 연속으로 최대 k번 더할 수 있다
-## 배열 안에 같은 수가 두번 이상 들어간 것은 다른 수로 간주한다 
 
 ## 수를 공백으로 구분하여 입력받기
 n, m, k = map(int, input().split())
@@ -34,7 +33,7 @@ second = data[-2]
 result = 0
 
 
-# 내 풀이
+## 내 풀이
 while True :
     if m == 0 :
         break
@@ -52,7 +51,7 @@ end_time = time.time()
 print('소요시간 : ', end_time - start_time)
 
 
-# 정답
+## 정답
 while True :
     for i in range(k) :
         if m == 0 :
