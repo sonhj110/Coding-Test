f4afe844140218b75e183c53818275e6d2c53e4b
hyojung <sonhj10318@gmail.com>
2023-10-09 21:28:50 +0900

    Code up 기초 100제 1일차(입출력)

---
+++ CodeUp6001_6005.py
@@ -0,0 +1,69 @@
+# 6001
+# python 언어에서 가장 기본적인 명령이 출력문이다.
+# print( )를 이용해 다음 단어를 출력하시오.
+
+# Hello
+
+print('Hello')
+
+
+
+
+
+# 6002
+# 이번에는 공백( )을 포함한 문장을 출력한다.
+# 다음 문장을 출력해보자.
+
+# Hello World
+# (대소문자에 주의한다.)
+
+print('Hello World')
+
+
+
+
+
+#6003
+# 이번에는 줄을 바꿔 출력하는 출력문을 연습해보자.
+# 다음과 같이 줄을 바꿔 출력해야 한다.
+
+# Hello
+# World
+# (두 줄에 걸쳐 줄을 바꿔 출력)
+
+
+print('Hello')
+print('World')
+
+print('''Hello
+World''')
+
+
+
+
+
+# 6004
+# 이번에는 작은 따옴표(')(single quotation mark)가 들어있는
+# 출력문 연습을 해보자.
+
+# 다음 문장을 출력하시오.
+
+# 'Hello'
+
+print("'Hello'")
+
+
+
+
+
+# 6005
+# 이번에는 큰따옴표(")(double quotation mark)가 포함된 출력문을 연습해보자.
+
+# 다음 문장을 출력하시오.
+
+# "Hello World"
+# (단, 큰따옴표도 함께 출력한다.)
+
+print('"Hello World"')
+
+
