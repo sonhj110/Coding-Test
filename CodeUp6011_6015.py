e9df67d831e583663c63c390176a9cde34091526
hyojung <sonhj10318@gmail.com>
2023-10-11 18:59:29 +0900

    Code up 기초 100제 3일차(입출력)

---
+++ CodeUp6011_6015.py
@@ -0,0 +1,67 @@
+# 6011
+# 변수에 실수값을 저장한 후
+# 변수에 저장되어 있는 값을 그대로 출력해보자.
+
+# 예시
+# f = input()
+# f = float(f)
+# print(f)
+# 와 같은 형태로 가능하다.
+
+f = input()
+f = float(f)
+print(f)
+
+
+
+
+
+# 6012
+# 줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+# 예시
+# a = input() 
+# b = input()
+# a=int(a)
+# b=int(b)
+# print(a)
+# print(b)
+# 과 같은 방법으로 가능하다.
+
+a = int(input())
+b = int(input())
+print(a)
+print(b)
+
+
+
+
+# 6013
+# 줄을 바꿔 문자(character) 2개를 입력받고, 순서를 바꿔 한 줄씩 출력해보자.
+
+a = input()
+b = input()
+print(b)
+print(a)
+
+
+
+
+
+# 6014
+# 실수(real number) 1개를 입력받아 줄을 바꿔 3번 출력해보자.
+
+f = float(input())
+print(f)
+print(f)
+print(f)
+
+
+
+
+# 6015
+# 공백을 두고 입력된정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+a, b = input().split()
+print(int(a))
+print(int(b))
